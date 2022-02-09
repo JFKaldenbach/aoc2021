f026439e4be98940907eb9227a8b40f46aa56443
Jelle Kaldenbach <jfkaldenbach@gmail.com>
2022-02-09 17:03:28 +0100

    Day 12

--- day12/script.js
+++ day12/script.js
@@ -1,87 +1,93 @@
 "use strict";
 
-const entries = `4743378318
-4664212844
-2535667884
-3273363861
-2282432612
-2166612134
-3776334513
-8123852583
-8181786685
-4362533174`;
+const entries = `LA-sn
+LA-mo
+LA-zs
+end-RD
+sn-mo
+end-zs
+vx-start
+mh-mo
+mh-start
+zs-JI
+JQ-mo
+zs-mo
+start-JQ
+rk-zs
+mh-sn
+mh-JQ
+RD-mo
+zs-JQ
+vx-sn
+RD-sn
+vx-mh
+JQ-vx
+LA-end
+JQ-sn`;
+
+const isLowercase = (string) => /[a-z]/.test(string);
+
+const mapping = {};
+
+entries.split("\n").map((entry) => {
+  const [e, f] = entry.split("-");
+  if (!mapping[e]) mapping[e] = [];
+  if (!mapping[f]) mapping[f] = [];
+  mapping[e].push(f);
+  mapping[f].push(e);
+});
 
 const part1 = function () {
-  // Increase energy of all by 1
-  // If value > 9
-  //// => flash
-  //// => increase energy of all surrounding by 1
-  //// Repeat this step
-  // Everything that flashed back to 0;
+  let paths = [];
 
-  const entriesArr = entries
-    .split("\n")
-    .map((entry) => entry.trim().split("").map(Number));
-
-  const flashStep = function (arr, row, column) {
-    if (arr[row][column] !== 0) {
-      arr[row][column]++;
+  const dfs = function (cave, path = []) {
+    path.push(cave);
+    if (cave === "end") {
+      paths.push(path);
+      return;
+    }
+    for (const partner of mapping[cave]) {
+      if (
+        (isLowercase(partner) && path.includes(partner)) ||
+        partner === "start"
+      ) {
+        continue;
+      }
+      dfs(partner, [...path]);
     }
   };
-  // deze 100 keer runnen
-  let flashes = 0;
-  for (let i = 0; i < 1000; i++) {
-    let flashesStep = 0;
-
-    for (let row = 0; row < entriesArr.length; row++) {
-      for (let column = 0; column < entriesArr[row].length; column++) {
-        if (entriesArr[row][column] < 10) {
-          entriesArr[row][column]++;
-        }
-      }
-    }
-
-    let flashed;
-    do {
-      flashed = false;
-      for (let row = 0; row < entriesArr.length; row++) {
-        for (let column = 0; column < entriesArr[row].length; column++) {
-          if (entriesArr[row][column] > 9) {
-            entriesArr[row][column] = 0;
-            if ([row - 1] >= 0 && [column - 1] >= 0) {
-              flashStep(entriesArr, [row - 1], [column - 1]);
-            }
-            if ([row - 1] >= 0) flashStep(entriesArr, [row - 1], [column]);
-            if ([row - 1] >= 0 && [column + 1] < entriesArr[row].length)
-              flashStep(entriesArr, [row - 1], [column + 1]);
-            if ([column + 1] < entriesArr[row].length)
-              flashStep(entriesArr, [row], [column + 1]);
-            if (
-              [column + 1] < entriesArr[row].length &&
-              [row + 1] < entriesArr.length
-            )
-              flashStep(entriesArr, [row + 1], [column + 1]);
-            if ([row + 1] < entriesArr.length)
-              flashStep(entriesArr, [row + 1], [column]);
-            if ([row + 1] < entriesArr.length && [column - 1] >= 0)
-              flashStep(entriesArr, [row + 1], [column - 1]);
-            if ([column - 1] >= 0) flashStep(entriesArr, [row], [column - 1]);
-            flashed = true;
-            flashesStep++;
-          }
-        }
-      }
-    } while (flashed);
-    if (flashesStep === entriesArr.flat(2).length) {
-      console.log(i + 1);
-      i = 1000;
-    }
-  }
+  dfs("start");
+  console.log(`Part 1: ${paths.length}`);
 };
 part1();
 
-const part2 = function () {};
+const part2 = function () {
+  let paths = [];
 
+  const dfs = function (cave, once, path = []) {
+    path.push(cave);
+
+    if (cave === "end") {
+      paths.push(path);
+      return;
+    }
+
+    for (const partner of mapping[cave]) {
+      if (partner === "start") continue;
+      if (isLowercase(partner) && path.includes(partner)) {
+        if (once) {
+          continue;
+        } else {
+          dfs(partner, true, [...path]);
+        }
+      } else {
+        dfs(partner, once, [...path]);
+      }
+    }
+  };
+  dfs("start", false);
+  console.log(`Part 2: ${paths.length}`);
+};
 part2();
 
 //
